70c28ce0c501b88d8b85ba240dce7e21fed1df2a
bubled33 <zimnitskiymisha@gmail.com>
2023-10-06 17:23:26 +0300

    Функционал анализа

--- instaproapi/schemas/fake.py
+++ instaproapi/schemas/fake.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from enum import Enum
+from typing import Dict
 
 from pydantic import BaseModel
 
@@ -8,6 +9,7 @@ class OutFake(BaseModel):
     username: str | None = None
     description: str | None = None
     subscribe_date: datetime | None = None
+    data: Dict[str, str] = {}
     id: str
 
 class InstanceTypes(str, Enum):
